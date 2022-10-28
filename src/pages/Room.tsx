f53db8e624efe74875fe4882eb597ba97567cee2
Lucas Mendonça <lucas.mendonca@neurobots.com.br>
2022-10-28 11:19:41 -0300

    foto de bg

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,33 +2,37 @@ import React from 'react';
 
 import { Heading, Stack, Input, Button, HStack, VStack } from '@chakra-ui/react';
 import { useHistory } from 'react-router-dom';
+import background from "../assets/bg2.jpg";
 
 const Room: React.FC = () => {
   const history = useHistory();
 
   return (
-    <Stack align="center" justify="center" flex={1} minH="100vh">
-      <Stack w={400} h={400} spacing='24px'>
-        <Stack>
-          <Heading as='h1' size='4xl'>CInfra</Heading>
-          <Heading as='h1' size='4xl'>Club</Heading>
+    <div style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+      <Stack align="center" justify="center" flex={1} minH="100vh">
+
+        <Stack w={400} h={400} spacing='24px'>
+          <Stack>
+            <Heading as='h1' size='4xl'>CInfra</Heading>
+            <Heading as='h1' size='4xl'>Club</Heading>
+          </Stack>
+          
+          <VStack>
+            <Input placeholder='Digite o código da sala' size="md" />
+          </VStack>
+
+          <HStack justify="space-between">
+            <Button onClick={() => history.push('/sala')} colorScheme='teal' size='md'>
+              Entrar na sala
+            </Button>
+
+            <Button onClick={() => history.push('/criar_sala')} colorScheme='teal' size='md'>
+              Criar sala
+            </Button> 
+          </HStack>
         </Stack>
-        
-        <VStack>
-          <Input placeholder='Digite o código da sala' size="md" />
-        </VStack>
-
-        <HStack justify="space-between">
-          <Button onClick={() => history.push('/sala')} colorScheme='teal' size='md'>
-            Entrar na sala
-          </Button>
-
-          <Button onClick={() => history.push('/criar_sala')} colorScheme='teal' size='md'>
-            Criar sala
-          </Button> 
-        </HStack>
       </Stack>
-    </Stack>
+    </div>
   );
 }
 
